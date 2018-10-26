a14d8fa0ef65c97aba56eab10128b746c18d42d8
linyingzhen <277715645@qq.com>
2018-10-26 17:04:05 +0800

    201810261704

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
+// module.exports = {
+//   exportPathMap: async function (defaultPathMap) {
+//     return {
+//       '/': { page: '/' },
+//       '/about': { page: '/about' },
+//       '/user/info': { page: '/user/info'},
+//       '/user/setinfo': { page: '/user/setinfo'},
+//     }
+//   }
+// }
+
+/* module.exports = {
+  exportPathMap(defaultPathMap) {
+    const pathMap = {};
+
+    Object.entries(defaultPathMap).forEach(([key, value]) => {
+      pathMap[key] = value;
+    });
+
+    return pathMap;
+  },
+}; */
+
 module.exports = {
-  exportPathMap: async function (defaultPathMap) {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/user/info': { page: '/user/info'},
-      '/user/setinfo': { page: '/user/setinfo'},
-    }
-  }
+  staticMarkup: true
 }
